a381bd80c0c5298ec9eecff6cbbe14a357b2344a
davidequaranta8 <d.quaranta8@studenti.unisa.it>
2025-05-27 17:58:18 +0200

    nasconde la tabBar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import MoveToDetail from './src/moveToDetail.js';
 import AddEdit from './src/addEdit.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 
 
 // Inizializzo il Tab Navigator
@@ -12,15 +13,21 @@ export default function App() {
   return (
     <NavigationContainer>
     <Tab.Navigator initialRouteName="Homepage">
-      <Tab.Screen name="Homepage" component={MoveToDetail} options={{/*con options possiamo personalizzare la tab.screen,la prima proprietà nasconde la scritta in alto invece la seconda definisce l'icona della tabBar sotto */ headerShown: false,tabBarIcon: ({ focused }) => (
-            <Image
-              source={require('./assets/tabIcon/homeIcon.png')}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          ) }}/>
+      <Tab.Screen name="Homepage" component={MoveToDetail} options={/* options è un obj che ci permette di personalizzare la tab.screen, definiamo una funzione per restituire tutte le proprietà imposte e per gestire il controllo per nascondere la tabBar*/
+      ({ route }) => {
+    const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';// permette di verificare qual'è la schermata mostrata (FOCUS), e se non è Home lascia routeName come undefined
+
+    return {
+      headerShown: false, /* nasconde la scritta in alto   */
+      tabBarIcon: ({ focused }) => ( /* imposta l'icona nella tabBar sotto */
+        <Image
+          source={require('./assets/tabIcon/homeIcon.png')}
+          style={{ width: 30, height: 30 }}
+        />
+      ),
+      tabBarStyle: routeName === 'Home' ? undefined : { display: 'none' }, /* controllo per nascondere la tabBar  */
+    };
+  }}/>
       <Tab.Screen name="---" component={AddEdit} options={{tabBarIcon: ({ focused }) => (
             <Image
               source={require('./assets/tabIcon/catIcon.png')}
