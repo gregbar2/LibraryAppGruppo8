c9eb5bb66b55011bcf491dd817b9559e18b59be0
davidequaranta8 <d.quaranta8@studenti.unisa.it>
2025-05-27 17:18:30 +0200

    sistemato navigator

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,Image } from 'react-native';
 import MoveToDetail from './src/moveToDetail.js';
 import AddEdit from './src/addEdit.js';
 import { NavigationContainer } from '@react-navigation/native';
@@ -11,9 +11,25 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={MoveToDetail} options={{ headerShown: false }}/>
-      <Tab.Screen name="---" component={AddEdit} />
+    <Tab.Navigator initialRouteName="Homepage">
+      <Tab.Screen name="Homepage" component={MoveToDetail} options={{/*con options possiamo personalizzare la tab.screen,la prima proprietà nasconde la scritta in alto invece la seconda definisce l'icona della tabBar sotto */ headerShown: false,tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('./assets/tabIcon/homeIcon.png')}
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          ) }}/>
+      <Tab.Screen name="---" component={AddEdit} options={{tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('./assets/tabIcon/catIcon.png')}
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          ) }}/>
     </Tab.Navigator>
   </NavigationContainer>
   );
--- src/moveToDetail.js
+++ src/moveToDetail.js
@@ -8,9 +8,9 @@ export default function MoveToDetail(){
 /* STACK NAVIGATOR serve per far si che quando clicco su un libro mi porta alla schermata di aggiunta o modifica */
         const Stack = createNativeStackNavigator();
         return(
-                <Stack.Navigator initialRouteName="Homescreen">
-                <Stack.Screen name="Homescreen" component={Homescreen} />
-                <Stack.Screen name="Aggiungi Modifica Libro" component={AddEdit} />
+                <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Homescreen} />
+                <Stack.Screen name="Aggiunta o Modifica Libro" component={AddEdit} />
                 <Stack.Screen name="Dettaglio" component={BookDetails} />
                 </Stack.Navigator>
 
