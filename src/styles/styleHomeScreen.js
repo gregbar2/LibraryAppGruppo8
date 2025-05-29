404bd2313ed610bc6f0b39244ba98ae0cc0de04d
gregbar2 <g.barberio7@studenti.unisa.it>
2025-05-29 10:54:09 +0200

    .

--- src/styles/styleHomeScreen.js
+++ src/styles/styleHomeScreen.js
@@ -6,46 +6,46 @@ const styleHomeScreen = StyleSheet.create({
     backgroundColor: '#ffffff',
     paddingHorizontal: 16,
     paddingTop: 40,
-    paddingBottom: 40,
+    paddingBottom: 40
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    marginBottom: 16
   },
   sectionTitle: {
     fontSize: 18,
     fontWeight: '600',
     marginTop: 24,
-    marginBottom: 12,
+    marginBottom: 12
   },
 
   bookItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 16,
+    marginBottom: 16
   },
   bookImage: {
     width: 50,
     height: 75,
     resizeMode: 'cover',
     borderRadius: 4,
-    marginRight: 12,
+    marginRight: 12
   },
   bookTextContainer: {
-    flex: 1,
+    flex: 1
   },
   bookTitle: {
     fontSize: 16,
-    fontWeight: '600',
+    fontWeight: '600'
   },
   bookAuthor: {
     fontSize: 14,
-    color: '#555',
+    color: '#555'
   },
   bookStatus: {
     fontSize: 12,
-    color: '#666',
+    color: '#666'
   },
 
   addButton: {
@@ -54,39 +54,44 @@ const styleHomeScreen = StyleSheet.create({
     borderRadius: 10,
     alignItems: 'center',
     marginTop: 24,
-    marginBottom: 20,
+    marginBottom: 20
   },
   addButtonText: {
     color: '#ffffff',
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
 
   suggestionList: {
     paddingHorizontal: 16,
-    gap: 12,
+    gap: 12
   },
   suggestionItem: {
     width: 100,
     marginRight: 12,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   suggestionImage: {
     width: 80,
     height: 120,
     borderRadius: 8,
-    marginBottom: 6,
+    marginBottom: 6
   },
   suggestionTitle: {
     fontSize: 13,
     fontWeight: '600',
-    textAlign: 'center',
+    textAlign: 'center'
   },
   suggestionAuthor: {
     fontSize: 11,
     color: '#555',
-    textAlign: 'center',
+    textAlign: 'center'
   },
+  placeholderText:{
+   textAlign: 'center',
+   marginTop: 20,
+   fontSize: 16
+  }
 });
 
 export default styleHomeScreen;
