f64ec8e7d211e0a9a88d3f71f3d56de3e43e7974
gregbar2 <g.barberio7@studenti.unisa.it>
2025-05-29 10:24:46 +0200

    .

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './src/navigation/AppNavigator.js';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AppTabs from './src/navigation/AppTabs.js';
+import BookDetails from './src/bookDetails.js';
+import EditBook from './src/editBook.js';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <Stack.Navigator>
+            <Stack.Screen name="Tabs" component={AppTabs} options={{ headerShown: false }} />
+            <Stack.Screen name="Dettaglio" component={BookDetails} />
+            <Stack.Screen name="Modifica Libro" component={EditBook} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
